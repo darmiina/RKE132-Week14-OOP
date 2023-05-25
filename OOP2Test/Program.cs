60df12b181510c8613dfa93bea9521818ecac847
darmiina <daia@tktk.ee>
2023-05-25 21:22:17 +0300

    Object-oriented programming

---
+++ OOP2Test/Program.cs
@@ -0,0 +1,52 @@
+﻿// Loo kassi klass. Kassil on nimi, värv ja omadus, mis näitab kasii näljasust.
+// Vaikimisi kassi loomisel, kassil on kõht täis (_bellyFull võrdub 10).
+// Peale loomist, kass läheb magama. Mida kauem kass magab, seda tühjemaks tal kõht läheb.
+// Kui _bellyFull jõuab nullini, siis kass ärkab üles ja ütleb "Meow".
+
+Cat newCat = new Cat ("Garfield", "ginger");
+Console.WriteLine($"{newCat.Name} wants a rub on its belly.");
+
+while (newCat.BellyFull > 0)
+{
+    newCat.Sleep();
+}
+newCat.Meow();
+newCat.ShowCatData();
+
+class Cat
+{
+    private string _name;
+    private string _color;
+    private int _bellyFull;
+public Cat(string name, string color)
+{
+    _name = name;
+        _color = color;
+        _bellyFull = 10;
+    Console.WriteLine($"A {color} cat {name} has been created.");
+}
+
+public string Name //getter
+    {  
+get  { return _name; }
+    }
+public int BellyFull //getter
+   { 
+    get { return _bellyFull; }
+    }
+    public void Sleep()
+{
+    _bellyFull--;
+    Console.WriteLine($"{_name} says: Z-z-Z-z-Z-z");
+}
+public void Meow()
+{
+    Console.WriteLine($"{_name} says: Meow!");
+}
+public void ShowCatData() //displays info about the cat object
+{
+    Console.WriteLine($"Name: {_name}");
+    Console.WriteLine($"Color: {_color}");
+    Console.WriteLine($"Level of hungriness: {_bellyFull}");
+}
+}
---
+++ Week14OOP/Program.cs
@@ -0,0 +1,57 @@
+﻿// Klassi loomine, objekti õpetama tegema midagi
+
+Console.WriteLine("Name your dog:");
+string myDogName = Console.ReadLine();
+Dog myDog = new Dog(myDogName);
+Dog neighboursDog = new Dog("Good Girl"); 
+
+Console.WriteLine($"My dogs name is {myDog.Name}.");
+Console.WriteLine($"My neighbours dogs name is {neighboursDog.Name}.");
+
+myDog.Rename("Bad Boy"); // Koera nime ümbernimetamiseks
+
+while (myDog.levelOfHappiness != 5)
+{
+    myDog.Bark();
+}
+
+myDog.WagTail();
+
+class Dog
+{
+    private string _name = "Koer"; //field
+    private int _levelOfHappiness; //field
+
+    // Constructor
+    public Dog(string name) // name - let the user name the dog
+    {
+        _name = name;
+        _levelOfHappiness = 0;
+    }
+    //getter
+    public string Name
+    {
+        get { return _name; }
+    }
+    // getter
+    public int levelOfHappiness
+    {
+        get { return _levelOfHappiness; }
+    }
+    public void Rename(string newName) // Meetod nime vahetamiseks
+    {
+        _name = newName;
+        Console.WriteLine($"The dog has been renamed to: {newName}");
+    }
+    public void Bark()
+    {
+        Console.WriteLine("Woof-woof!");
+        _levelOfHappiness++;
+    }
+
+    public void WagTail()
+    {
+        Console.WriteLine("Wiggle-wiggle!");
+    }
+}
+
